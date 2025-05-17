817467301fc699db4f258b6a5fa6716d035a816f
Sahith <sahithkautilya@gmail.com>
2025-05-17 10:05:19 +0530

    shell-practice

---
+++ 03.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+User1=$1
+User2=$2
+
+echo "$User1: Hi $User2, how are you?"
+echo "$User2: I am good $User1. where are you going?"
+echo "$User1: I am going to Hyderabad. Will you join with me?"
+echo "$User2: Yes please."
