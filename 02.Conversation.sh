66d671c2ec9f2c903ad5c1a13454ad2018ffd09c
Sahith <sahithkautilya@gmail.com>
2025-05-17 09:53:27 +0530

    shell-practice

--- 02.Conversation.sh
+++ 02.Conversation.sh
@@ -3,7 +3,7 @@
 User1=Sahith
 User2=Divya
 
-echo"$User1: Hi $User2, how are you?"
-echo"$User2: I am good $User1. where are you going?"
-echo"$User1: I am going to Hyderabad. Will you join with me?"
-echo"$User2: Yes please."
+echo "$User1: Hi $User2, how are you?"
+echo "$User2: I am good $User1. where are you going?"
+echo "$User1: I am going to Hyderabad. Will you join with me?"
+echo "$User2: Yes please."
