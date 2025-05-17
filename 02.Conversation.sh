158ba788481cb5077e27790b089c36db76cbc9fd
Sahith <sahithkautilya@gmail.com>
2025-05-17 09:47:24 +0530

    variables

---
+++ 02.Conversation.sh
@@ -0,0 +1,6 @@
+User1=Sahith
+User2=Divya
+$User1: Hi $User2, how are you?
+$User2: I am good $User1. where are you going?
+$User1: I am going to Hyderabad. Will you join with me?
+$User2: Yes please.
