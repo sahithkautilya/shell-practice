b36b131ee996b5a9fa319d687d2e1561b19f4257
Sahith <sahithkautilya@gmail.com>
2025-05-17 11:12:17 +0530

    shell-practice

--- 06.array.sh
+++ 06.array.sh
@@ -4,4 +4,4 @@ DevOpsTools=(Jenkins Maven , Ansible Terraform "Docker", "Kubetnetes", "Grafana"
 
 echo "Pipeline tool: ${DevOpsTools[0]}"
 echo "IAC tool:      ${DevOpsTools[2]}"
-echo "All DevOps Tools:${DevOpsTools{@}}"
+echo "All DevOps Tools:${DevOpsTools[@]}"
